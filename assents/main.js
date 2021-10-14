7599acdca458fb278b93f8ecf65c6715bcd74221
Andretreccia <andreabtreccia@gmail.com>
2021-10-14 15:51:42 +0200

    situazione di stallo
    Analisi del ptoblema  e della possibile struttura del programma

--- assents/main.js
+++ assents/main.js
@@ -1,8 +1,15 @@
 //Mail
 
     //creare archivio mail
+const registeredMail = ["1@gmail.com", "2@gmail.com", "3@gmail.com", "4@gmail.com"]
+console.log(registeredMail);
     //chiedere all utente la propria mail
+const userMail = prompt("Inserisci qui la tua mail")
+console.log(userMail)
     //verificare che la mail inseritra dall utente sia presente in archivio
+for (let i = 0; i <= registeredMail.length; i++) {
+
+}
         //determinare due condizioni e rispettivi eventi
 
 
